5bf62412cc7aa11d69518fdbd3e7c173fe0d3e99
Mahmudul Islam <mahmud612@gmail.com>
2017-05-25 16:12:40 +1000

    initial commit

---
+++ AtlSimpleInjectorMultiConstructorResolver.Test/ConstructorResolutionTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Atl.SimpleInjector.MultiConstructorResolver;
+using Xunit;
+
+namespace AtlSimpleInjectorMultiConstructorResolver.Test
+{
+    public class ConstructorResolutionTest : IClassFixture<MultiConstructorResolutionBehavior>
+    {
+        private readonly MultiConstructorResolutionBehavior _constructorResolutionBehavior;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstructorResolutionTest"/> class.
+        /// </summary>
+        /// <param name="constructorResolutionBehavior">The constructor resolution behavior.</param>
+        public ConstructorResolutionTest(MultiConstructorResolutionBehavior constructorResolutionBehavior)
+        {
+            _constructorResolutionBehavior = constructorResolutionBehavior;
+        }
+        #region Referenced Classes
+        public class A
+        {
+        }
+
+        public class B
+        {
+            public B()
+            {
+
+            }
+        }
+
+        public class C
+        {
+            public C() : this(0)
+            {
+
+            }
+
+            public C(int a) : this(0, 0)
+            {
+
+            }
+
+            public C(int a, int b)
+            {
+
+            }
+        }
+
+        public class D
+        {
+            public D(int a)
+            {
+
+            }
+
+            public D(string a)
+            {
+
+            }
+        }
+        #endregion
+
+        [Fact]
+        public void ResolveNoConstructor()
+        {
+            var result = _constructorResolutionBehavior.GetConstructor(typeof(A));
+            Assert.NotNull(result);
+        }
+        
+        [Fact]
+        public void ResolveOneConstructor()
+        {
+            var result = _constructorResolutionBehavior.GetConstructor(typeof(B));
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void ResolveMulitpleConstructorWithUnEqualParamters()
+        {
+            var result = _constructorResolutionBehavior.GetConstructor(typeof(C));
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void ResolveMulitpleConstructorWithEqualParamters()
+        {
+            Assert.Throws<Exception>(() => _constructorResolutionBehavior.GetConstructor(typeof(D)));
+        }
+    }
+}
---
+++ AtlSimpleInjectorMultiConstructorResolver/MultiConstructorResolutionBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleInjector.Advanced;
+
+namespace Atl.SimpleInjector.MultiConstructorResolver
+{
+    /// <summary>
+    /// Constructor resolver that return the constructor that has teh highest number of parameters
+    /// </summary>
+    /// <seealso cref="IConstructorResolutionBehavior" />
+    public class MultiConstructorResolutionBehavior : IConstructorResolutionBehavior
+    {
+        /// <summary>
+        /// Gets the constructor.
+        /// </summary>
+        /// <param name="implementationType">Type of the implementation.</param>
+        /// <returns></returns>
+        public ConstructorInfo GetConstructor(Type implementationType)
+        {
+            var constructors = implementationType.GetConstructors().OrderByDescending(x => x.GetParameters().Length);
+            var constructor = constructors.FirstOrDefault();
+            var totalParameters = constructor?.GetParameters().Length;
+            if (constructors.Count(x => x.GetParameters().Length == totalParameters) > 1)
+                throw new Exception($"Type {implementationType.Namespace} has multiple constructors defined with same number of paramters.");
+            return constructor;
+        }
+    }
+}
